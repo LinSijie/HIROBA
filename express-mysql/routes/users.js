409e5eca658b633f3f9a14c3243733a2c1c2df37
LinSijie <linsijie17@163.com>
2018-11-30 13:37:50 +0800

    install express-mysql-session

--- express-mysql/routes/users.js
+++ express-mysql/routes/users.js
@@ -7,8 +7,45 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/addUser', function(req, res, next){
-  userDao.add(req, res, next);
+/* Add user. */
+router.post('/addUser', function(req, res, next) {
+	userDao.add(req, res, next);
+});
+
+/* Update user. */
+router.post('/updateUser', function(req, res, next) {
+	userDao.update(req, res, next);
+});
+
+/* Delete user. */
+router.post('/delUser', function(req, res, next) {
+	userDao.delete(req, res, next);
+});
+
+/* Query by Id. */
+router.post('/queryById', function(req, res, next) {
+	userDao.queryById(req, res, next);
+});
+
+/* Query by Username. */
+router.post('/queryByUsername', function(req, res, next) {
+	userDao.queryByUsername(req, res, next);
+});
+
+/* Query All. */
+router.post('/queryAll', function(req, res, next) {
+	userDao.queryAll(req, res, next);
+});
+
+/* Login */
+router.post('/login', function(req, res, next){
+	userDao.login(req, res, next);
+});
+
+/* Logout */
+router.post('/logout', function (req, res, next) {
+	req.session.username = null; // delete session
+	res.redirect('/'); // TODO: change it to msg
 });
 
 module.exports = router;
--- express-mysql/src/userDao.js
+++ express-mysql/src/userDao.js
@@ -28,6 +28,121 @@ const add = (req, res, next) => {
 	});
 };
 
+/**
+ * Update a user
+ * 
+ * The request must include all elements. So the best way for front end is query
+ * first and then update it.
+ */
+const update = (req, res, next) => {
+    if (req.body.id === undefined) {
+        utils.jsonHelper(res, undefined);
+    }
+
+    pool.getConnection(function(err, connection){
+        const param = [req.body.username, 
+                       req.body.password, 
+                       req.body.class, 
+                       req.body.email,
+                       req.body.id];
+        
+        connection.query(tables.users.update, param, function(err, result) {
+            if (result) {
+                result = utils.msg.SUCCESS_MSG;    
+            }
+
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
+/**
+ * Delete a user
+ */
+const del = (req, res, next) => {
+    pool.getConnection(function(err, connection) {
+        const id = req.body.id;
+        connection.query(tables.users.delete, id, function(err, result) {
+            if (result) {
+                result = utils.msg.SUCCESS_MSG;
+            }
+
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
+/**
+ * Query by Id
+ */
+const queryById = (req, res, next) => {
+    pool.getConnection(function(err, connection) {
+        const id = req.body.id;
+        connection.query(tables.users.queryById, id, function(err, result) {
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
+/**
+ * Query by username
+ */
+const queryByUsername = (req, res, next) => {
+    pool.getConnection(function(err, connection) {
+        const username = req.body.username;
+        connection.query(tables.users.queryUsername, username, function(err, result) {
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
+/**
+ * Query all
+ */
+const queryAll = (req, res, next) => {
+    pool.getConnection(function(err, connection) {
+        connection.query(tables.users.queryAll, function(err, result) {
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
+/**
+ * Login
+ */
+const login = (req, res, next) => {
+    pool.getConnection(function(err, connection) {
+        const username = req.body.username;
+        connection.query(tables.users.queryByUsername, username, function(err, result) {
+            if (result) {
+                let [ userInfo ] = result;
+                if (userInfo.password === req.body.password) {
+                    req.session.username = req.body.username; // success, setup session
+
+                    result = utils.msg.SUCCESS_MSG;
+                } else {
+                    result = utils.msg.INVALID_PWD;
+                }
+            } else {
+                result = utils.msg.INVALID_USER;
+            }
+            utils.jsonHelper(res, result);
+            connection.release();
+        });
+    });
+};
+
 module.exports = { 
-	add: add
+	add: add,
+	update: update,
+    delete: del,
+    queryById: queryById,
+    queryByUsername: queryByUsername,
+	queryAll: queryAll,
+	login: login
 };
